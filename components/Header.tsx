f001f9ba4f1860db04c618309cfad7114e573cac
hyemin <hm_stom@naver.com>
2021-09-02 18:25:35 +0900

    #User menu style

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,13 @@ import Link from "next/link";
 import Logo from "@/assets/svg/logo/Logo.svg";
 import TextLogo from "@/assets/svg/logo/TextLogo.svg";
 import palette from "styles/palette";
-import SignUpModal from "@/components/SignUpModal";
 import useModal from "@/hooks/useModal";
 import { useSelector } from "../store";
 import HamburgerIcon from "@/assets/svg/Icon/HamburgerIcon.svg";
 import { useDispatch } from "react-redux";
 import { authAction } from "@/store/auth";
 import AuthModal from "./auth/AuthModal";
-
+import OutsideClickHandler from "react-outside-click-handler";
 const Container = styled.div`
   background-color: white;
   position: sticky;
@@ -92,6 +91,40 @@ const Container = styled.div`
       z-index: 11;
     }
   }
+
+  /* react-outside-click-handler div */
+  /* div태그가 생기는데 이 태그에는 className안 먹힘 따라서 형제 연산자 적용 */
+  .header-logo-wrapper + div {
+    position: relative;
+  }
+  .header-usermenu {
+    position: absolute;
+    right: 0;
+    top: 52px;
+    width: 240px;
+    padding: 8px 0;
+    box-shadow: 0 2px 16px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    background-color: white;
+
+    li {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 42px;
+      padding: 0 16px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-usermenu-divider {
+      width: 100%;
+      height: 1px;
+      margin: 8px 0;
+      background-color: ${palette.gray_dd};
+    }
+  }
 `;
 const Header: React.FC = () => {
   // 모달 value 가져오기
@@ -99,6 +132,7 @@ const Header: React.FC = () => {
   const user = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
+  const [isUsermenuOpen, setIsUsermenuOpen] = useState(false);
 
   return (
     <Container>
@@ -132,11 +166,39 @@ const Header: React.FC = () => {
         </div>
       )}
 
+      {/* outsideclickhandler 적용 */}
       {user.isLogged && (
-        <button className="header-user-profile" type="button">
-          <HamburgerIcon />
-          <img src={user.profileImage} className="header-user-profile-image" alt="" />
-        </button>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            if (isUsermenuOpen) {
+              setIsUsermenuOpen(false);
+            }
+          }}
+        >
+          <button className="header-user-profile" type="button" onClick={() => setIsUsermenuOpen((prev) => !prev)}>
+            <HamburgerIcon />
+            <img src={user.profileImage} className="header-user-profile-image" alt="" />
+          </button>
+          {isUsermenuOpen && (
+            <ul className="header-usermenu">
+              <li>숙소관리</li>
+              <Link href="/room/register/building">
+                <a
+                  role="presentation"
+                  onClick={() => {
+                    setIsUsermenuOpen(false);
+                  }}
+                >
+                  <li>숙소 등록하기</li>
+                </a>
+              </Link>
+              <div className="header-usermenu-divider" />
+              <li role="presentation" onClick={() => {}}>
+                로그아웃
+              </li>
+            </ul>
+          )}
+        </OutsideClickHandler>
       )}
       {/* Modal 출력 */}
       <ModalPortal>
