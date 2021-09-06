e649187ddb97fe1227e93541cf297bd81fbf4241
hyemin <hm_stom@naver.com>
2021-09-06 15:27:59 +0900

    #Google API load map

--- types/map.d.ts
+++ types/map.d.ts
@@ -8,3 +8,33 @@ declare global {
     initMap: () => void;
   }
 }
+
+window.initMap = () => {
+  //* 지도 불러오기
+  if (mapRef.current) {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: {
+        lat: latitude || 37.5666784,
+        lng: longitude || 126.9778436,
+      },
+      zoom: 14,
+    });
+    const marker = new window.google.maps.Marker({
+      position: {
+        lat: latitude || 37.5666784,
+        lng: longitude || 126.9778436,
+      },
+      map,
+    });
+    map.addListener(
+      "center_changed",
+      throttle(() => {
+        const centerLat = map.getCenter().lat();
+        const centerLng = map.getCenter().lng();
+        marker.setPosition({ lat: centerLat, lng: centerLng });
+        dispatch(registerRoomActions.setLatitude(centerLat));
+        dispatch(registerRoomActions.setLongitude(centerLng));
+      }, 150),
+    );
+  }
+};
