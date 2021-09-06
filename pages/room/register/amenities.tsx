41ee47a6f8885c298798d4cf7b48d4af4699cdd8
hyemin <hm_stom@naver.com>
2021-09-06 15:29:54 +0900

    Revert "#편의시설 & common checkbox components 추가"
    This reverts commit cc4facb4ccb8c55c6089e1b7d93b760808b41d6d.

--- pages/room/register/amenities.tsx
+++
@@ -1,8 +0,0 @@
-import RegisterRoomAmenities from "@/components/room/register/RegisterRoomAmenities";
-import React from "react";
-
-const amenities = () => {
-  return <RegisterRoomAmenities />;
-};
-
-export default amenities;
