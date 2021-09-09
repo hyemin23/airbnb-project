0f3b755f854a74f09dedb3e0545c2317fcd08ef9
hyemin <hm_stom@naver.com>
2021-09-09 13:21:50 +0900

    #Room list page & Google Map API render

---
+++ components/room/main/RoomList.tsx
@@ -0,0 +1,33 @@
+import { useSelector } from "@/store/index";
+import React from "react";
+import styled, { css } from "styled-components";
+import RoomCard from "./RoomCard";
+
+const Container = styled.ul<{ showMap: boolean }>`
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: 50px;
+  width: 100%;
+
+  ${({ showMap }) =>
+    showMap &&
+    css`
+      flex-direction: column;
+    `}
+`;
+
+interface IProps {
+  showMap: boolean;
+}
+const RoomList: React.FC<IProps> = ({ showMap }) => {
+  const rooms = useSelector((state) => state.room.rooms);
+  return (
+    <Container showMap={showMap}>
+      {rooms.map((room) => (
+        <RoomCard room={room} showMap={showMap} key={room.id} />
+      ))}
+    </Container>
+  );
+};
+
+export default RoomList;
